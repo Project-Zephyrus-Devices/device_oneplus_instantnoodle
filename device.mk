8e24f7fa1de231f1ee09b18a91ce06bfaa35e5a5
zahid <zahidhasmani07@gmail.com>
2022-09-19 00:04:46 +0000

    Revert "instantnoodle: Remove OOS 11 sensor configs"
    This reverts commit fd9f2d5f144b029c1d9da1316150924f72590419.

--- device.mk
+++ device.mk
@@ -49,10 +49,6 @@ PRODUCT_COPY_FILES += \
 
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
     
 # fstab
 PRODUCT_PACKAGES += \
